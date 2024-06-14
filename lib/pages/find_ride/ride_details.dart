b0a902331552c122912faf0f7552bbff060cedc5
Sivuziwe Nogubha <snogubha023a@student.wethinkcode.co.za>
2024-06-14 13:13:10 +0200

    fixed the confirm lift page

--- lib/pages/find_ride/ride_details.dart
+++ lib/pages/find_ride/ride_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../main.dart';
+
 class ConfirmRidePage extends StatelessWidget {
   final LatLng departureLocation;
   final LatLng destinationLocation;
@@ -20,78 +22,143 @@ class ConfirmRidePage extends StatelessWidget {
     required this.departure,
   });
 
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Confirm Ride'),
-        // backgroundColor: Colors.transparent,
+    double midLatitude = (departureLocation.latitude + destinationLocation.latitude) / 2;
+    double midLongitude = (departureLocation.longitude + destinationLocation.longitude) / 2;
+    double adjustedMidLatitude = midLatitude + 0.0050;
+
+    final confirmButton = Material(
+      elevation: 4,
+      borderRadius: BorderRadius.circular(30),
+      color: primary,
+      child: MaterialButton(
+        onPressed: () {
+          //
+        },
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        child: Text(
+          "Confirm Ride",
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        textColor: Colors.white,
       ),
-      body: Column(
+    );
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   backgroundColor: const Color(0x44000000),
+      //   title: Text(
+      //     'Confirm Ride',
+      //     style: TextStyle(
+      //       color: Colors.black,
+      //       fontSize: 20,
+      //       fontWeight: FontWeight.w500,
+      //       decoration: TextDecoration.none,
+      //       fontFamily: 'Aeonik',
+      //     ),
+      //   ),
+      // ),
+      body: Stack(
         children: [
-          Expanded(
-            child: GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                  (departureLocation.latitude + destinationLocation.latitude) / 2,
-                  (departureLocation.longitude + destinationLocation.longitude) / 2,
-                ),
-                zoom: 12,
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(
+                adjustedMidLatitude,
+                midLongitude,
               ),
-              markers: {
-                Marker(
-                  markerId: MarkerId('departure'),
-                  position: departureLocation,
-                  infoWindow: InfoWindow(title: 'Departure Location'),
-                ),
-                Marker(
-                  markerId: MarkerId('destination'),
-                  position: destinationLocation,
-                  infoWindow: InfoWindow(title: 'Destination Location'),
-                ),
+              zoom: 13,
+            ),
+            markers: {
+              Marker(
+                markerId: MarkerId('departure'),
+                position: departureLocation,
+                infoWindow: InfoWindow(title: 'Departure Location'),
+              ),
+              Marker(
+                markerId: MarkerId('destination'),
+                position: destinationLocation,
+                infoWindow: InfoWindow(title: 'Destination Location'),
+              ),
+            },
+            polylines: {
+              Polyline(
+                polylineId: PolylineId('route'),
+                points: [departureLocation, destinationLocation],
+                color: Colors.blue,
+                width: 4,
+              ),
+            },
+          ),
+          Positioned(
+            top: 25.0,
+            left: 16.0,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
               },
-              polylines: {
-                Polyline(
-                  polylineId: PolylineId('route'),
-                  points: [departureLocation, destinationLocation],
-                  color: Colors.blue,
-                  width: 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.8), // Greyish background color with opacity
+                  shape: BoxShape.rectangle, // Shape of the container (circular in this case)
                 ),
-              },
+                padding: EdgeInsets.all(8.0),
+                child: ImageIcon(
+                  AssetImage('assets/back.png'),
+                  color: Colors.white, // Color of the icon (white)
+                ),
+              ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text('Trip Details'),
-                Text('Departure: ${departure}'),
-                Text('Destination: ${destination}'),
-                // Display user details
-                Text('Offered By: $offeredByName'),
-                // Display user profile picture
-                CircleAvatar(
-                  backgroundImage: NetworkImage(offeredByPhotoUrl),
+          Positioned(
+            bottom: 16.0,
+            left: 16.0,
+            right: 16.0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Trip Details', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                      SizedBox(height: 8.0),
+                      Text('Departure: $departure'),
+                      Text('Destination: $destination'),
+                      SizedBox(height: 8.0),
+                      Text('Offered By: $offeredByName'),
+                      SizedBox(height: 8.0),
+                      CircleAvatar(
+                        radius: 40, // Adjust the radius as needed for the desired size
+                        backgroundColor: Colors.grey[300], // Optional background color for the avatar
+                        backgroundImage: NetworkImage(
+                          offeredByPhotoUrl,
+                        ),
+                        child: ClipOval(
+                          child: Image.network(
+                            offeredByPhotoUrl,
+                            fit: BoxFit.contain, // Ensure the image fits perfectly within the CircleAvatar
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      confirmButton,
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Cancel'),
+                      ),
+                    ],
+                  ),
                 ),
-                // Display trip details here
-                // Offered by, price, etc.
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement booking functionality
-                    // Navigate to confirmation page or perform booking action
-                  },
-                  child: Text('Confirm Ride'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context); // Navigate back to FindRide page
-                  },
-                  child: Text('Cancel'),
-                ),
-              ],
+              ),
             ),
           ),
         ],
