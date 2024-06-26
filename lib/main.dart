8b94b3128bff80988dcbb20a53acc430cb1e9c9d
Sivuziwe Nogubha <snogubha023a@student.wethinkcode.co.za>
2024-06-26 17:32:51 +0200

    New branch

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,8 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 }
 
 
+
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: "./.env");
